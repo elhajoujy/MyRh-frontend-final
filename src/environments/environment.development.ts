f67beff59c1a4f0be5eac0c6642f203e766985e9
e.elhjoujy <e.elhjoujy@labelvie.ma>
2024-02-13 15:02:06 +0100

    IMPORTING THE HOST-URL FROM THE URL , environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production : false,
-  backendHost: 'http://localhost:8080',
-  hostSocket: 'http://localhost:8080/',
-  stripe:'pk_test_51OXkyCLRYmZMwYWSf3TbgvnfNWE8F6KQ8IRwVA4F5Ty3hVggKSEf7saH7BVUa5QxeCj6qByHYiDsCRQAv4IsexUw00ncTsASSI'
+  production: false,
+  backendHost: 'http://16.170.236.106:8080',
+  hostSocket: 'http://16.170.236.106:8080',
+  stripe: 'pk_test_51OXkyCLRYmZMwYWSf3TbgvnfNWE8F6KQ8IRwVA4F5Ty3hVggKSEf7saH7BVUa5QxeCj6qByHYiDsCRQAv4IsexUw00ncTsASSI'
 };
