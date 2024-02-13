053debb25281ec805be5d025f5b16c7f7af1cdff
e.elhjoujy <e.elhjoujy@labelvie.ma>
2024-02-13 16:23:32 +0100

    deploy-frontend-to-ec2

--- src/app/store/common/App.effects.ts
+++ src/app/store/common/App.effects.ts
@@ -1,23 +1,25 @@
-import { Injectable } from '@angular/core';
-import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { emptyAction, showAlert } from './App.Action';
-import { exhaustMap, map } from 'rxjs';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import {Injectable} from '@angular/core';
+import {Actions, createEffect, ofType} from '@ngrx/effects';
+import {emptyAction, showAlert} from './App.Action';
+import {exhaustMap, map} from 'rxjs';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Injectable()
 export class AppEffects {
-  constructor(private actions$: Actions, private _snackbar: MatSnackBar) {}
+  constructor(private actions$: Actions, private _snackbar: MatSnackBar) {
+  }
+
 
   _showAlert = createEffect(() =>
     this.actions$.pipe(
       ofType(showAlert),
       exhaustMap((action) => {
         return this.showsnackBarAlert(action.message, action.resultType).afterDismissed()
-        .pipe(
+          .pipe(
             map(() => {
-                return emptyAction()
+              return emptyAction()
             })
-        );
+          );
       })
     )
   );
