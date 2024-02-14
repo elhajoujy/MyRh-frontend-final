673cc1d6a3b73b26aff396cd64c521e7fe488243
fadwa2022 <fadwacherqui@gmail.com>
2024-02-14 14:19:32 +0100

    test quiz job seeker  serviceand profile quez service

--- src/app/service/jobSeeker/quiz-job-seeker/quiz-job-seeker.service.spec.ts
+++ src/app/service/jobSeeker/quiz-job-seeker/quiz-job-seeker.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { QuizJobSeekerService } from './quiz-job-seeker.service';
+import { environment } from '../../../../environments/environment';
 
 describe('QuizJobSeekerService', () => {
   let service: QuizJobSeekerService;
@@ -13,4 +14,23 @@ describe('QuizJobSeekerService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+})
+
+it('should send quiz result', () => {
+  const httpClientSpy = jasmine.createSpyObj('HttpClient', ['post']);
+  const service = new QuizJobSeekerService(httpClientSpy);
+
+  const jobseekerId = 1;
+  const currentDate = '2024-02-14';
+  const validated = true;
+
+  service.sendQuizResult(jobseekerId, currentDate, validated);
+
+  expect(httpClientSpy.post).toHaveBeenCalledWith(`${environment.backendHost}/myrh/api/v1/jobSeekers/results`, {
+    jobseekerId: jobseekerId,
+    Datepassedexam: currentDate,
+    isvalidated: validated,
+  });
 });
+
+;
