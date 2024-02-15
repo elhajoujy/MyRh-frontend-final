2ef717aca9bc940d317570fee01a04b29384fa58
e.elhjoujy <e.elhjoujy@labelvie.ma>
2024-02-15 10:21:26 +0100

    changes port backend host

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  //TODO:IT'S A BAD PRACTICE TO HARDCODE THE URLS
   backendHost: 'http://51.20.32.21:8080',
   hostSocket: 'http://51.20.32.21:8080',
   stripe: 'pk_test_51OXkyCLRYmZMwYWSf3TbgvnfNWE8F6KQ8IRwVA4F5Ty3hVggKSEf7saH7BVUa5QxeCj6qByHYiDsCRQAv4IsexUw00ncTsASSI'
